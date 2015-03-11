9c068c8bd842e2f5463d4d987e63e0eced69ea74
Antonio <rantoniops@gmail.com>
2015-03-11 14:37:46 -0700

    Initial Commit

---
+++ mySafari/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  mySafari
+//
+//  Created by Antonio Perez on 3/11/15.
+//  Copyright (c) 2015 antonioperez. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ mySafari/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  mySafari
+//
+//  Created by Antonio Perez on 3/11/15.
+//  Copyright (c) 2015 antonioperez. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
